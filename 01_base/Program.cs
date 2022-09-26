567a46dbbc8fc277f00dcc5a000667ab0e121f51
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2022-09-26 14:05:01 +0300

    Add project files.

---
+++ 01_base/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+
+namespace _01_base
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Data Types: 
+                * reference types (classes) - can be null
+                * value types (structures)  - cannot be null
+            */
+
+            int number = 10;
+            string str = "hello";
+
+            object obj = new object();
+            obj.ToString();
+
+            // Snippet: cw+TAB+TAB
+            Console.WriteLine();
+
+            Console.WriteLine("My number is " + number.ToString() + "!");
+            Console.WriteLine($"My number is {number}!"); // interpolation
+
+            string email = null; // empty reference
+            //email.ToLower();   // error
+
+            //Nullable<decimal> salary = null;
+            decimal? salary = null;
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your height: ");
+
+            float height = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Hello dear {name} your height: {height}cm");
+
+            if (5 > 10)
+            {
+                // if condition return true
+            }
+            else
+            {
+                // if condition return false
+            }
+
+            switch (height)
+            {
+                case 0: // code...
+                    break;
+                default:
+                    break;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+
+            }
+
+            while (true)
+            {
+
+            }
+
+            do
+            {
+
+            } while (true);
+        }
+    }
+}
---
+++ 02_files/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace _02_files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            string text = File.ReadAllText($"{desktopPath}/123.txt");
+
+            Console.WriteLine($"Text length: {text.Length}");
+
+            string[] words = text.Split(' ', ',', '.', '!', '?');
+            Console.WriteLine($"Words count: {words.Length}");
+        }
+    }
+}
