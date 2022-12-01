fe9ed2abe0bfeda0fa0d5b1a8ae148272dc94f23
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2022-12-01 13:38:41 +0200

    First MVC Application.

---
+++ 02_first_mvc_app/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _02_first_mvc_app.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            //...
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
---
+++ 02_first_mvc_app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _02_first_mvc_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
