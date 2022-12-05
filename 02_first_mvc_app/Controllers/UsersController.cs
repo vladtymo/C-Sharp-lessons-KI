673ffe7080d80268acb24a9b05ffb7cfa85756a9
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2022-12-05 13:40:15 +0200

    User list with delete button

--- 02_first_mvc_app/Controllers/UsersController.cs
+++ 02_first_mvc_app/Controllers/UsersController.cs
@@ -1,18 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _02_first_mvc_app.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace _02_first_mvc_app.Controllers
 {
     public class UsersController : Controller
     {
+        static private List<User> users = new List<User>()
+        {
+            new User() {Id = 1, Name = "Bob", Email = "saper@gmail.com", BirthDate = DateTime.Now},
+            new User() {Id = 2, Name = "Vlad", Email = "blabla@gmail.com", BirthDate = DateTime.Now},
+            new User() {Id = 3, Name = "John", Email = "mister@ukr.net", BirthDate = DateTime.Now},
+            new User() {Id = 4, Name = "Lilia", Email = "developer@gmail.com", BirthDate = DateTime.Now}
+        };
+
         public IActionResult Index()
         {
             //...
-            return View();
+            return View(users);
         }
 
         public IActionResult Profile()
         {
             return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            var user = users.FirstOrDefault(x => x.Id == id);
+
+            if (user == null) return NotFound(); // 404
+
+            users.Remove(user);
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ 02_first_mvc_app/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace _02_first_mvc_app.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
